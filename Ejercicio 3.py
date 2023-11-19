f4a0b625042bcbbcfd15a252f23e182f8a561011
Andrepepa <150764630+Andrepepa@users.noreply.github.com>
2023-11-19 03:04:22 +0000

    Subiendo ejercicio 3

---
+++ Ejercicio 3.py
@@ -0,0 +1,6 @@
+peso_payaso=112
+peso_muñeca=75
+numero_payasos=int(input('¿Cual es el numero de payasos a enviar?:'))
+numero_muñecas=int(input('¿Cual es el numero de muñecas a enviar?:'))
+peso_total=(peso_payaso*numero_payasos)+(peso_muñeca*numero_muñecas)
+print(f'El peso total del paquete que sera enviado es:{peso_total} g')
