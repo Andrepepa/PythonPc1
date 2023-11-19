5d5e7750c84b82e721c101e7a9600a7b5366b952
Andrepepa <150764630+Andrepepa@users.noreply.github.com>
2023-11-19 05:37:17 +0000

    Subiendo ejercicio 10

---
+++ Ejercicio 10.py
@@ -0,0 +1,5 @@
+colores=['rojo','verde','blanco','negro','rosa','amarillo' ]
+colores.remove('rojo')
+colores.remove('rosa')
+colores.remove('amarillo')
+print(f'La nueva lista depues de eliminar los colores de la posicion 0,4 y 5 es:{colores}')
